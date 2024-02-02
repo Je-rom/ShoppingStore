eb69e9d6f91a7af15cf6bdd95afb3b70c3f40551
Jerry Abadi <abadijerry44@gmail.com>
2024-02-02 17:02:48 +0100

    Add project files.

---
+++ CouponServices/Data/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CouponServices.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+    }
+}
---
+++ CouponServices/Models/Coupon.cs
@@ -0,0 +1,11 @@
+﻿namespace CouponServices.Models
+{
+    public class Coupon
+    {
+        public int CouponId { get; set; }
+        public string CouponCode { get; set; }
+        public double DiscountAmount { get; set; }
+        public int MinAmount { get;}
+
+    }
+}
