6d7407733b2dbd9b16257fcfdee308c8e359dab5
Jerry Abadi <abadijerry44@gmail.com>
2024-02-29 17:56:51 +0100

    CartContoller

---
+++ CouponWeb/Controllers/CartController.cs
@@ -0,0 +1,21 @@
+﻿using CouponWeb.Service.IService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CouponWeb.Controllers
+{
+    public class CartController : Controller
+    {
+
+        private readonly ICartService _cartService;
+        public CartController(ICartService cartService)
+        {
+            _cartService = cartService;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
