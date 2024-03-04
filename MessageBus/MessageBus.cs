beafcdc9806df186756efc7e26fe5f588117f9e6
Jerry Abadi <abadijerry44@gmail.com>
2024-03-04 16:18:35 +0100

    serviceBus

--- MessageBus/MessageBus.cs
+++ MessageBus/MessageBus.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Azure.Messaging.ServiceBus;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace MessageBus
 {
-    internal class MessageBus
+    public class MessageBus : IMessageBus
     {
+
+        private string _connectionString = "Endpoint=sb://shoppingproject.servicebus.windows.net/;SharedAccessKeyName=service;SharedAccessKey=OyXkqd7cIcttCS+qZRqmewn1AObUDMRZK+ASbDVhwVg=;EntityPath=emailshopping";
+        public async Task PublishMessage(object message, string topic_queue_name)
+        {
+            await using var client = new ServiceBusClient(_connectionString);
+        }
     }
 }
