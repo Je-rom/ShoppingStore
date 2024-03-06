2d37e2e5ba18d49fefd30b632e816ea3aad1abce
Jerry Abadi <abadijerry44@gmail.com>
2024-03-06 18:05:28 +0100

    EmailApi

---
+++ EmailApi/Extension/ApplicationBuilderExtensions.cs
@@ -0,0 +1,31 @@
+﻿using EmailApi.Messaging;
+
+namespace EmailApi.Extension
+{
+    public static class ApplicationBuilderExtensions
+    {
+        private static IAzureServiceBusConsumer ServiceBusConsumer { get; set; }
+        public static IApplicationBuilder UseAzureServiceBusConsumer(this IApplicationBuilder app)
+        {
+            ServiceBusConsumer = app.ApplicationServices.GetService<IAzureServiceBusConsumer>();
+            var hostApplicationLife = app.ApplicationServices.GetService<IHostApplicationLifetime>();
+
+            hostApplicationLife.ApplicationStarted.Register(OnStart);
+            hostApplicationLife.ApplicationStopping.Register(OnStop);
+
+            return app;
+        }
+
+        private static void OnStop()
+        {
+            ServiceBusConsumer.Stop();
+        }
+
+        private static void OnStart()
+        {
+            ServiceBusConsumer.Start();
+        }
+
+
+    }
+}
--- EmailApi/Program.cs
+++ EmailApi/Program.cs
@@ -1,4 +1,5 @@
 using EmailApi.Data;
+using EmailApi.Messaging;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddSingleton<IAzureServiceBusConsumer, AzureServiceBusConsumer>(); // dont want a new object for different requests/ no duplicate
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
